89c204e570fe09da043d9bb66495a6b2e5ce6446
FatHeroNwhy <60132895+FatHeroNwhy@users.noreply.github.com>
2023-06-05 21:38:34 +0300

    Modal windows added

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -4,12 +4,14 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 import { NavLink } from "react-router-dom";
-import { SHOP_ROUTE } from "../utils/consts";
+import { ADMIN_ROUTE, LOGIN_ROUTE, SHOP_ROUTE } from "../utils/consts";
 import { Button } from "react-bootstrap";
 import { observer } from "mobx-react-lite"
+import { useNavigate } from 'react-router-dom';
 
 export const NavBar = observer(() => {
   const { user } = useContext(Context);
+  const history = useNavigate()
 
   return (
     <Navbar bg="dark" variant="dark">
@@ -19,8 +21,8 @@ export const NavBar = observer(() => {
         </NavLink>
         {user.isAuth ? 
           <Nav className="ml-auto" style={{ color: "white" }}>
-            <Button variant={"outline-light"}>Админ Панель</Button>
-            <Button variant={"outline-light"} className="ms-4">Войти</Button>
+            <Button variant={"outline-light"} onClick = {() => history(ADMIN_ROUTE)}>Админ Панель</Button>
+            <Button variant={"outline-light"} onClick = {() => history(LOGIN_ROUTE)} className="ms-4">Выйти</Button>
           </Nav>
          : 
           <Nav className="ml-auto" style={{ color: "white" }}>
